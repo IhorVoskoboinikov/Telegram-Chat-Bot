3184bd975dbbc2672e2d3b66f1a7bb2d57c11873
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-08-10 12:06:52 +0300

    Чат бот первый этап: Приветствие с работающими стартовыми кнопками!

--- bot_dev.py
+++ bot_dev.py
@@ -1,14 +1,16 @@
 import telebot
+from text_responses import *
 
 from telebot import types
-TOKEN ='2125399793:AAFWEjDTdXVPkerpBXp4JzMuTwNWKeURTYU'
+
+TOKEN = '2125399793:AAFWEjDTdXVPkerpBXp4JzMuTwNWKeURTYU'
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f'Привет, {message.from_user.first_name} {message.from_user.last_name}! \n' \
-           f'Это фитнес клуб "GYM for people"!\n\n' \
+           f'Это фитнес клуб "Tsarsky City Resort "!\n\n' \
            f'Мы рады что ты выбрал именно нас для улучшения своей физической формы!\n\n' \
            f'Мы готовы ответить на все твои вопросы, выбери раздел:'
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
@@ -22,43 +24,12 @@ def start(message):
     bot.send_message(message.chat.id, mess, reply_markup=markup)
 
 
-# @bot.message_handler(content_types=['text'])
-# def get_user_text(message):
-#     if message.text == 'Привет':
-#         bot.send_message(message.chat.id, 'И тебе привет', parse_mode='html')
-#     elif message.text == 'id':
-#         bot.send_message(message.chat.id, f'Твой ID: {message.from_user.id} ', parse_mode='html')
-#     elif message.text == 'photo':
-#         photo = open('like_icon.jpg', 'rb')
-#         bot.send_photo(message.chat.id, photo)
-#     else:
-#         bot.send_message(message.chat.id, 'Я тебя не понимаю)))', parse_mode='html')
-#
-#
-# @bot.message_handler(content_types=['photo'])
-# def get_user_photo(message):
-#     bot.send_message(message.chat.id, f'{message.from_user.last_name}, классное фото!')
-#
-#
-# @bot.message_handler(commands=['website'])
-# def website(message):
-#     markup = types.InlineKeyboardMarkup()
-#     markup.add(types.InlineKeyboardButton('Жми на меня!', url='https://www.youtube.com'))
-#     bot.send_message(message.chat.id, f'{message.from_user.last_name}, перейди на youtube!', reply_markup=markup)
-#
-#
-# @bot.message_handler(commands=['help'])
-# def website(message):
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-#     website = types.KeyboardButton('/website')
-#     start = types.KeyboardButton('/start')
-#     markup.add(website, start)
-#     bot.send_message(message.chat.id, 'OK', reply_markup=markup)
-
+@bot.message_handler(content_types=['text'])
+def get_user_text(message):
+    if message.text in ['Общая информация', 'Клубные карты', 'Записаться на тренировку', 'Время работы клуба',
+                        'Связаться с менеджером']:
+        bot.send_message(message.chat.id, text_responses[message.text], parse_mode='html')
+    else:
+        bot.send_message(message.chat.id, 'Нужно выбрать из перечисленный вариантов!', parse_mode='html')
 
 bot.polling(none_stop=True)
-
-
-
-
-
