b096d7a84ed29da2045543d7cfee4061e3be32f3
Ihor_Voskoboinikov <voskoboinikov@gmail.com>
2022-09-06 10:43:17 +0300

    Bot config:

---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+TOKEN_FILE_PATH = 'token.txt'
+ID_MANAGER_FILE_PATH = 'manager_id.txt'
+DB_FILE_PATH = 'clients.db'
+BASE_CLUB_CARDS_FILE_PATH = 'base_cards.xlsx'
+TRAINING_SCHEDULE_FILE_PATH = 'trainings.xlsx'
+SIGN_UP_FOR_TRAININGS_FILE_PATH = 'records.xlsx'
+WORKOUT_TYPES_FILE_PATH = 'trainings_types.xlsx'
+COACHES_FILE_PATH = 'coaches.xlsx'
+HELLO_STICKER_FILE_PATH = 'images/hello_sticker.webp'
+TRAINING_STICKER_RECORDS_FILE_PATH = 'images/training_sticker.webp'
+FINAL_CLUB_CARDS_STICKER_FILE_PATH = 'images/final_sticker.webp'
+TIME_PUSH_MESSAGE_END_CARDS = "06:00"
+TIME_PUSH_MESSAGE_RECORDS_TRAINING = "12:00"
+
